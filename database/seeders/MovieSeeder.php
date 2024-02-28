f28233827de049441f99a7888a49000ea61abb5e
Drew0004 <drdrewgraphic@gmail.com>
2024-02-28 17:07:39 +0100

    add seeder

---
+++ database/seeders/MovieSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Movie;
+
+class MovieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            // Istanzio il model
+            $movie = new movie();
+            // Ne riempio le colonne
+            $movie->title = fake()->words(rand(1, 5), true);
+            $movie->author = fake()->name();
+            $movie->genre = fake()->words(rand(1,2), true);
+            $movie->release_year = fake()->date('Y_m_d').' '.fake()->time();
+            $movie->description = fake()->paragraphs();
+            // Lo salvo in persistenza
+            $movie->save();
+        }
+    }
+}
