b464873b054bc8f6d452678edbe810271978f4df
MironAlberto <alberto.miron19@gmail.com>
2024-02-28 16:39:00 +0100

    Add Book Route and Book Controller

--- app/Http/Controllers/Guest/BookController.php
+++ app/Http/Controllers/Guest/BookController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        /* $books = Book::all(); */
+        return view('subpages.book', compact('books'));
+    } 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Guest\MovieController;
+use App\Http\Controllers\Guest\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/movie',[MovieController::class,'movie'])->name('movie');
+
+Route::get('/book', [BookController::class, 'book'])->name('book');
 
 // Route::get(PERCORSO CON CUI ARRIVARE ALLA PAGINA, FUNZIONE DI CALLBACK CHE MI CREA LA RISPOSTA DA DARE ALL UTENTE)
