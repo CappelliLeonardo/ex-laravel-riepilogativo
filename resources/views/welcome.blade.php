0798c1cf3db43a0a34939b0566a9fdcab59bf7a0
Drew0004 <drdrewgraphic@gmail.com>
2024-02-28 16:42:02 +0100

    Made buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,10 @@
         <div class="col-12">
             <h1>HomePage</h1>
             <button class="btn btn-primary ">
-                <a class="text-decoration-none text-white" href="#">Vai a Movies</a>
+                <a class="text-decoration-none text-white" href="{{ '/movie' }}">Vai a Movies</a>
             </button>
             <button class="btn btn-primary ">
-                <a class="text-decoration-none text-white" href="#">Vai a Books</a>
+                <a class="text-decoration-none text-white" href="{{ '/book' }}">Vai a Books</a>
             </button>
         </div>
     </div>
